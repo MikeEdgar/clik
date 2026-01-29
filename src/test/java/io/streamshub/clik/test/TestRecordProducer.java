875928306bf90da379546388731a2b1b44a56090
Michael Edgar <medgar@redhat.com>
2026-01-29 09:40:01 -0500

    Increase producer linger in tests to increase batch sizes
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- src/test/java/io/streamshub/clik/test/TestRecordProducer.java
+++ src/test/java/io/streamshub/clik/test/TestRecordProducer.java
@@ -70,6 +70,7 @@ public interface TestRecordProducer {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServers());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.LINGER_MS_CONFIG, "50");
 
         CountDownLatch counter = new CountDownLatch(records.length);
         Function<TestRecord, Integer> partition;
